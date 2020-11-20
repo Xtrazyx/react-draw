12f4c7e47acff45e3b1ede4bf98dc6eb97155778
Xtrazyx <xtrazyx@gmail.com>
2020-11-20 10:47:45 +0100

    add styled components

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,6 +1,17 @@
 import * as React from 'react';
 import { Scene } from "./scene";
 import {Square} from "./square";
+import styled from 'styled-components';
+
+const Container = styled.div`
+    position: relative
+`;
+
+const Button = styled.button`
+    position: absolute;
+    bottom: 20px;
+    left: 20px;
+`;
 
 export default function App(){
     const [xPos, setXPos] = React.useState(10);
@@ -10,12 +21,12 @@ export default function App(){
     }
 
     return (
-        <div>
-            <button onClick={moveRight}>Move Right</button>
+        <Container>
+            <Button onClick={moveRight}>Move Right</Button>
             <Scene height={500} width={500} style={{backgroundColor: '#41b1f7'}}>
-              <Square x={xPos} y={10} size={30}/>
+              <Square x={xPos} y={10} size={30} color={'#9e8d39'}/>
               <Square x={50} y={10} size={30}/>
             </Scene>
-        </div>
+        </Container>
     )
 }
--- src/components/square.tsx
+++ src/components/square.tsx
@@ -6,14 +6,16 @@ interface Props {
     x?: number,
     y?: number,
     size?: number,
+    color?: string
 }
 
 export const Square: FunctionComponent<Props> = (props)  => {
-    const { size = 0, x = 0, y = 0 } = props;
+    const { size = 0, x = 0, y = 0, color = '#000000' } = props;
 
     const ctx: CanvasRenderingContext2D = React.useContext(CanvasContext);
 
     React.useEffect(() => {
+        ctx.fillStyle = color;
         ctx.fillRect(x, y, size, size);
     });
 
